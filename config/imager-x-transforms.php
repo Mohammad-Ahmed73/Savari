92f5c2009e1aeec9bc7c564ca50c3d7f58c7eb5c
Ahmed <Naiyaab@nkdigitalsolutions.com>
2025-04-29 16:57:42 +0500

    Savari Project

---
+++ config/imager-x-transforms.php
@@ -0,0 +1,52 @@
+<?php
+
+return [
+    'noRatio' => [
+        'transforms' => [
+            ['width' => 400],
+            ['width' => 800],
+            ['width' => 1200]
+        ],
+        'defaults' => []
+    ],
+    '1x1' => [
+        'transforms' => [
+            ['width' => 400],
+            ['width' => 800],
+            ['width' => 1200]
+        ],
+        'defaults' => [
+            'ratio' => 1/1
+        ]
+    ],
+    '4x3' => [
+        'transforms' => [
+            ['width' => 400],
+            ['width' => 800],
+            ['width' => 1200]
+        ],
+        'defaults' => [
+            'ratio' => 4/3
+        ]
+    ],
+    '5x3' => [
+        'transforms' => [
+            ['width' => 400],
+            ['width' => 800],
+            ['width' => 1200]
+        ],
+        'defaults' => [
+            'ratio' => 5/3
+        ]
+    ],
+    '16x9' => [
+        'transforms' => [
+            ['width' => 400],
+            ['width' => 800],
+            ['width' => 1200]
+        ],
+        'defaults' => [
+            'ratio' => 16/9
+        ]
+    ]
+];
